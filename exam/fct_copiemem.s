8bfb52c9d6701f16887f49640aa06dc9f90a8066
Morgane DELMAS <morgane.delmas@ensimag.grenoble-inp.fr>
2019-05-31 10:59:14 +0200

    exercice 2 question 2

--- exam/fct_copiemem.s
+++ exam/fct_copiemem.s
@@ -51,25 +51,27 @@ copiemem2:
     // sauvegarde registres non scratch
     pushl %ebx
     pushl %esi
+    pushl %edi
     // copie valeurs des paramètres dans des registres
     movl 8(%ebp), %eax
     movl 12(%ebp), %ecx
     movl 16(%ebp), %edx
     // uint32_t i = 0
     movl $0, %esi
-for:
+for2:
     // for (i < taille)
     cmpl %eax, %esi
-    jge end_for
+    jge end_for2
     // src[i]
     movb (%ecx, %esi, 1), %bl
     // dst[i] = src[i]
     movb %cl, (%eax, %esi, 1)
     // i++
     addl $1, %esi
-    jmp for
-end_for:
+    jmp for2
+end_for2:
     // restauration registres non scratch
+    popl %edi
     popl %esi
     popl %ebx
     leave
