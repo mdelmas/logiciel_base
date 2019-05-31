71c991f2283eead90f44be351c9693060d53add1
Morgane DELMAS <mdelmas93@gmail.com>
2019-05-31 10:34:56 +0200

    exercice 2, question 1

---
+++ exam/fct_copiemem.s
@@ -0,0 +1,37 @@
+/*
+void copiemem0(uint8_t *dst, uint8_t *src, uint32_t taille)
+{
+    for (uint32_t i = 0; i < taille; i++) {
+        dst[i] = src[i];
+    }
+}
+*/
+
+    .text
+    .globl copiemem0
+    // void copiemem0(uint8_t *dst, uint8_t *src, uint32_t taille)
+    // uint8_t *dst : 8(%ebp)
+    // uint8_t *src : 12(%ebp)
+    // uint32_t taille : 16(%ebp)
+copiemem0:
+    // uint32_t i : -4(%ebp)
+    enter $4, $0
+    // uint32_t i = 0
+    movl $0, -4(%ebp)
+for:
+    // for (i < taille)
+    movl 16(%ebp), %eax
+    cmpl %eax, -4(%ebp)
+    jge end_for
+    // src[i]
+    movl 12(%ebp), %eax
+    movl -4(%ebp), %edx
+    movb (%eax, %edx, 1), %cl
+    // dst[i] = src[i]
+    movl 8(%ebp), %eax
+    movb %cl, (%eax, %edx, 1)
+    addl $1, -4(%ebp)
+    jmp for
+end_for:
+    leave
+    ret
