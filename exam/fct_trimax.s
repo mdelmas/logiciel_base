64da3bf8fc48f4a750a5eb82b4357be5883fb138
Morgane DELMAS <mdelmas93@gmail.com>
2019-05-31 09:25:20 +0200

    question 1 exam

--- exam/fct_trimax.s
+++ exam/fct_trimax.s
@@ -24,24 +24,22 @@ struct cellule_t {
     // struct cellule_t *creer_liste1(uint16_t tab[], uint32_t taille)
     .globl creer_liste1
     // uint16_t tab[] : %rdi -> -28(%rbp)  (64 bits)
-    // uint32_t taille : %esi -> -36(%rbp) (32 bits)
 creer_liste1:
     // struct cellule_t *liste : -8(%rbp)  (64 bits)
-    // int32_t i : -16(%rbp)               (32 bits)
+    // int32_t i : -12(%rbp)               (32 bits)
     // struct cellule_t *cell : -20(%rbp)  (64 bits)
-    enter $48, $0
+    enter $32, $0
     // sauvegarde paramètres
     movq %rdi, -28(%rbp)
-    movl %esi, -36(%rbp)
     // liste = NULL
     movq $0, -8(%rbp)
     // i = taille - 1
     movl %esi, %eax
     subl $1, %eax
-    movl %eax, -16(%rbp)
+    movl %eax, -12(%rbp)
 for:
     // for i >= 0
-    cmpl $0, -16(%rbp)
+    cmpl $0, -12(%rbp)
     jnae end_for
     // struct cellule_t *cell = malloc(sizeof(struct cellule_t))
     movq $16, %rdi
@@ -49,7 +47,7 @@ for:
     movq %rax, -20(%rbp)
     // cell->val = tab[i]
     movq -28(%rbp), %rdx
-    movl -16(%rbp), %eax
+    movl -12(%rbp), %eax
     movw (%rdx, %rax, 2), %ax
     movw %ax, -20(%rbp)
     // cell->suiv = liste
@@ -59,7 +57,7 @@ for:
     movq -20(%rbp), %rax
     movq %rax, -8(%rbp)
     // i--
-    subl $1, -16(%rbp)
+    subl $1, -12(%rbp)
 end_for:
     // return liste
     movq -8(%rbp), %rax
